001f098d41823267ff553dc1b40e7e045cea5bd6
Muhammad Ali Murtaza <muhammadalimurtaza997@gmail.com>
2022-11-08 14:37:04 +0500

    basic running form of server

---
+++ server/src/configs/db.js
@@ -0,0 +1,11 @@
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    } catch (err) {
+      console.log(`Error: ${err.message}`.red);
+      process.exit(1);
+    }
+}
+
+module.exports = connectDB;
