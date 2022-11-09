e9904fcf603bcf66a1eeba3e3a0ae0daeb773457
Muhammad Ali Murtaza <muhammadalimurtaza997@gmail.com>
2022-11-09 20:44:51 +0500

    Basic files created for API creation

---
+++ server/src/models/productModel.js
@@ -0,0 +1,1 @@
+// Define Schema for the product here
---
+++ server/src/routes/productRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const router = express.Router()
+
+// Import Product Controllers here and then route them as get, post, put or delete
