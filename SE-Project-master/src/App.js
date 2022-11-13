91d8954c961dce5c9bc730489e5bc5905b671256
Noor Atif <nooratif97@gmail.com>
2022-11-13 23:31:43 +0500

    Blogs Page Added

--- SE-Project-master/src/App.js
+++ SE-Project-master/src/App.js
@@ -1,7 +1,11 @@
 import Login from './container/auth/login'
 import Cart from './container/cart'
 import Policy from './container/Policies'
+import Blogs from './container/Blogs'
 
-const App = () => <Policy />
+
+
+
+const App = () => <Blogs />
 
 export default App
