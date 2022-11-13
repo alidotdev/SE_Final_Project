cb4de4ab22cc59e9e2d2872ad999f8a9e8f92a59
Noor Atif <nooratif97@gmail.com>
2022-11-13 23:07:31 +0500

    Exchange & Return Policy Page Added

--- SE-Project-master/src/App.js
+++ SE-Project-master/src/App.js
@@ -1,6 +1,7 @@
 import Login from './container/auth/login'
 import Cart from './container/cart'
+import Policy from './container/Policies'
 
-const App = () => <Cart />
+const App = () => <Policy />
 
 export default App
---
+++ SE-Project-master/src/container/Policies/index.jsx
@@ -0,0 +1,69 @@
+import { Footer, Header } from "../../components";
+import "./styles.css";
+
+const Policy = () => {
+  return (
+    <>
+      <Header />
+
+      <div>
+        <h1 style={{ display: "flex", justifyContent: "center" }}>Exchange & Return Policy</h1>
+      </div>
+      <div className="Main">
+        <p className="question">1-How Return Works?</p>
+        <p className="answer">
+        After you’ve submitted a return request to customercare@sparrowbia.pk or call us at +92-42-111128733. After reviewing your request, if the article is delivered wrong from our side then we’ll get it picked from your doorstep within 7 to 10 working days and you will not be charged for shipping. Once the article is received at our Warehouse and after checking the quality, we’ll issue an online credit that can be used for online shopping on SparrowBia.
+        </p>
+    
+        <p className="question">2-What Is Online Store Credit??</p>
+        <p className="answer">
+        Online store credit is digital money at your SparrowBia account. It means you can return your purchase and utilize the same amount for a later purchase.
+        </p>
+        <p className="question">
+          3-Are There Any Limitations To The Products I Can Exchange?
+        </p>
+        <p className="answer">
+        SparrowBia makes sure that all their customers are satisfied so we offer you the opportunity to exchange a product for another if the need arises. However, there are a few limitations as to how you can avail this opportunity.
+        The brand does not offer exchanges for accessories.
+Please note that items on sales and promotions are not eligible for exchange.
+Once sales start the final price at the sale will be the final price for any exchange.
+The product is in a re-saleable condition.
+Please make sure that all tags and items are intact and that the garment is unused.
+        </p>
+        <p className="question">4-Can I Get Cash Refund Instead?</p>
+        <p className="answer">
+        SparrowBia does not offer any cash refunds. We do offer online store credit or the chance to exchange your product for another, depending on the situation.
+        </p>
+        <p className="question">
+          5-Exchange Policy For Damaged Jewelry Or Wrong Jewelry Article Delivered (ONLY FOR ONLINE ORDERS)?
+        </p>
+        <p className="answer">
+        If the Jewelry article is damaged or wrong article is delivered to the customer, it can be exchanged within 7 days from the nearest stores by showing the receipt. In case of the cities where the store is not available, customer will be offered a coupon of an equivalent amount. Validity of coupon will be for 30 days.
+Tag attached to the Jewelry needs to be intact in order for the item to be exchanged.
+Jewelry will only be delivered nationwide. Will not be shipped to international customers.
+        </p>
+        <p className="question">6-Exchange Policy For Perfumes (ONLY FOR ONLINE ORDERS)?</p>
+        <p className="answer">
+        The perfumes cannot be exchanged if the seal/packaging of the perfume box is opened or damaged in any way.
+        </p>
+        <p className="question">7-How Do I Go About Exchanging A Product?</p>
+        <p className="answer">
+        Simply email us at customercare@sparrowbia.pk and request an exchange, and we will guide you through the process. Don’t forget to mention your order ID in the email.
+        </p>
+        <p className="question">
+          8-How do I contact customer services department?
+        </p>
+        <p className="answer">
+          You can contact us by any of the following methods:
+        </p>
+        <ul className="answer">
+          <li>By WhatsApp: +92 312 4922033</li>
+          <li>By using our Website contact form</li>
+          <li>By sending email to sparrowbia@gmail.com</li>
+        </ul>
+      </div>
+      <Footer />
+    </>
+  );
+};
+export default Policy;
